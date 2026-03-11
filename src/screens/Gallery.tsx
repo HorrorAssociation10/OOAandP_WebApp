e3f53d7e735619e0dee07bcd3ec0096431bb690f
Daniel_Firefox <81545604+HorrorAssociation10@users.noreply.github.com>
2026-03-11 19:59:36 +1000

    IGotTired
    Название коммита говорит само за себя... Это просто контрольная точка перед выходом)

--- src/screens/Gallery.tsx
+++ src/screens/Gallery.tsx
@@ -1,8 +1,34 @@
+import {useState} from "react"
+
 export default function Gallery(){
-    return (
-        <div>
-            <p>Seems like you've got no projects yet... Let's fix that!</p>
-            <p>Hit the "Create Project" button in the top left corner</p>
-        </div>
-    )
+    const [projects, setProjects] = useState(0);
+
+    function CreateProjectButtonPressed(){
+        setProjects(projects+1);
+    }
+
+    if (!projects){
+        return (
+            <div>
+                <button onClick={CreateProjectButtonPressed}>Projects Created: {projects}</button>
+                <p>Seems like you've got no projects yet... Let's fix that! <br/>
+                Hit the "Create Project" button in the top left corner</p>
+
+            </div>
+        )
+    }
+    else {
+        return (
+            <div>
+                <button onClick={CreateProjectButtonPressed}>Projects Created: {projects}</button>
+                <div className="grid grid-cols-1 md:grid-cols-3">
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                    <p>hi</p>
+                </div>
+            </div>
+        )
+    }
+    
 }
