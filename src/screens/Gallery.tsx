a4af995a539390f93a8065f06a61c6db2a51d98b
Daniel_Firefox <81545604+HorrorAssociation10@users.noreply.github.com>
2026-03-11 18:22:48 +1000

    RoutingCreated
    Добавлен базовый роутинг и страницы-заглушки (ну... пока что они пустые, поэтому так)

---
+++ src/screens/Gallery.tsx
@@ -0,0 +1,8 @@
+export default function Gallery(){
+    return (
+        <div>
+            <p>Seems like you've got no projects yet... Let's fix that!</p>
+            <p>Hit the "Create Project" button in the top left corner</p>
+        </div>
+    )
+}
---
+++ src/screens/NotFound.tsx
@@ -0,0 +1,5 @@
+export default function NotFound(){
+    return (
+        <h1>Sorry, this page either does not exist or was moved to a dfferent directory</h1>
+    )
+}
