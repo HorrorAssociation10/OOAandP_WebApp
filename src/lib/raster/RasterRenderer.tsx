49bd5b97f767586a475dd073e906f5f6e3c2ecee
Daniel_Firefox <81545604+HorrorAssociation10@users.noreply.github.com>
2026-03-25 20:37:56 +1000

    AnotherBlastOfEnergy
    Еще один случайный всплеск энергии... Если можно так выразиться, конечно.
    Конвертация цвета из HEX в rgba, усечение байта, рисование прямой Брезенхема

--- src/lib/raster/RasterRenderer.tsx
+++ src/lib/raster/RasterRenderer.tsx
@@ -3,12 +3,29 @@ export type RGBA = {r: number, g: number, b: number, a: number};
 export type LineAlg = 'bresenham' | 'wu';
 
 export function clampByte(v: number): number {
-    throw new Error('Not implemented: clampByte');
+    if (v<0)
+        v = 0;
+    else if (v > 255)
+        v = 255;
+
+    return v;
+    // throw new Error('Not implemented: clampByte');
 }
 
 export function hexToRGBA(hex: string, alpha = 255): RGBA {
-    // Не успел
-    throw new Error('Not implemented hexToRGBA');
+    let hexCode: string = hex.replace('#', '');
+    
+    if (hex.length == 3){
+        hexCode = hexCode[0]+hexCode[0] + hexCode[1]+hexCode[1] + hexCode[2]+hexCode[2];
+    }
+
+    const red = parseInt(hexCode.substring(0, 2), 16),
+          green = parseInt(hexCode.substring(2, 4), 16),
+          blue = parseInt(hexCode.substring(4, 6), 16);
+
+    const color: RGBA = {r: red, g: green, b: blue, a: alpha};
+    return color;
+    // throw new Error('Not implemented hexToRGBA');
 }
 
 export class RasterRenderer {
@@ -74,7 +91,15 @@ export class RasterRenderer {
         // throw new Error('Not implemented: setColor');
     }
 
+    // Это плохой blendPixel, ибо у него деление (StraightAlpha)
     private blendPixel(x: number, y: number, color: RGBA, alphaFactor = 1) {
+        const buf = this.buf;
+        const index = this.idx(x, y);
+
+        const colorDest: RGBA = {r: buf[index], g: buf[index+1], b: buf[index+2], a: buf[index+3]};
+        const alphaOut = color.a + colorDest.a*(1-color.a);
+
+        //Тут я пока что так и не додумал
         throw new Error('Not implemented: blendPixel');
     }
 
@@ -91,7 +116,27 @@ export class RasterRenderer {
     }
 
     drawLineBrassenham(x0: number, y0: number, x1: number, y1: number, color: RGBA) {
-        throw new Error('Not implemented: drawLineBrassenham');
+        alert("Method works");
+        const delta_x = Math.abs(x1-x0);
+        const delta_y = Math.abs(y1-y0);
+        const sign_x = (x0 < x1) ? 1 : -1;
+        const sign_y = (y0 < y1) ? 1 : -1;
+
+        let err = delta_x - delta_y;
+        while (x0 != x1 || y0 != y1) {
+            this.setPixel(x0, y0, color);
+            let e2 = 2*err;
+            if (e2 >= -delta_y){
+                x0 += sign_x;
+                err -= delta_y;
+            }
+            if (e2 <= delta_x){
+                y0 += sign_y;
+                err += delta_x;
+            }
+        }
+        // Шиш знает, рабочий или нет... А проверить пока не получилось
+        // throw new Error('Not implemented: drawLineBrassenham');
     }
 
     drawLineWu(x0: number, y0: number, x1: number, y1: number, color: RGBA) {
